84b80801a8c513acc9d7b7673e6ccc2b84a795f3
HusterYP <3209605462@qq.com>
2018-07-26 01:01:41 +0800

    Lesson_2

--- Algorithms/Lesson_1/Lesson_1/src/test/Demo.java
+++ Algorithms/Lesson_1/Lesson_1/src/test/Demo.java
@@ -1,25 +1,13 @@
 package test;
 
 
-import source.Stopwatch;
-import source.StopwatchCPU;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Demo {
     public static void main(String[] args) {
-        Stopwatch stopwatch = new Stopwatch();
-        StopwatchCPU stopwatchCPU = new StopwatchCPU();
-        int data[] = {40, -10, -30, 30, -20, 100, 80, -90};
-        int num = 1000;
-        int count = 0;
-        for (int i = 0; i < num; i++)
-            for (int j = 0; j < num; j++)
-                for (int k = 0; k < num; k++) {
-                    if (data[i % data.length] + data[j % data.length] + data[k % data.length] == 0) {
-                        count++;
-                    }
-                }
-        double time = stopwatch.elapsedTime();
-        double cputTime = stopwatchCPU.elapsedTime();
-        System.out.println(time + " ; " + cputTime);
+        List list = new ArrayList();
+        list.add("temp");
+        list.add(0);
     }
 }
